2d3e5f916cb0e3a7237645b649b5299ed1cb123b
iBaino <baino@airqo.net>
2020-08-19 21:18:28 +0300

    organisers update

--- organizers.php
+++ organizers.php
@@ -30,6 +30,15 @@ require 'header-left-bar.php'; //THIS MUST BE INCLUDED FOR EACH PAGE
 		</div>
 	</div>
 
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/regina.jpg);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Regina Hebig</div>
+			<div class="role">General Chair</div>
+			<div class="affiliation">Chalmers and University of Gothenburg, Sweden</div>
+		</div>
+	</div>
+	
 	<div class="spacer"></div>
 	
 	<div class="organizer-group-title">Organising Committee</div>
@@ -99,6 +108,144 @@ require 'header-left-bar.php'; //THIS MUST BE INCLUDED FOR EACH PAGE
 		</div>
 	</div>
 
+	<div class="spacer"></div>
+	
+	<div class="organizer-group-title">Program Committee</div>
+	
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/person.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Loek Cleophas</div>
+			<div class="affiliation">TU Eindhoven, The Netherlands; and Stellenbosch University, South Africa </div>
+		</div>
+	</div>
+	
+	
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/person.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Yirsaw Ayalew</div>
+			<div class="affiliation">University of Botswana, Botswana</div>
+		</div>
+	</div>
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/person.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Ibrahim Adeyanju</div>
+			<div class="affiliation">Federal University Oye-Ekiti, Nigeria</div>
+		</div>
+	</div>
+	
+
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/person.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Rafael Capilla</div>
+			<div class="affiliation">Universidad Rey Juan Carlos, Madrid, Spain</div>
+		</div>
+	</div>
+
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/person.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Micheal Tuape</div>
+			<div class="affiliation">University of Botswana, Botswana</div>
+		</div>
+	</div>
+	
+	
+
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/person.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Eric Umuhoza</div>
+			<div class="affiliation">Carnegie Mellon University Africa, Rwanda</div>
+		</div>
+	</div>
+	
+
+	
+
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/person.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Salome Maro</div>
+			<div class="affiliation">University of Gothenburg, Sweden</div>
+		</div>
+	</div>
+	
+
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/regina.jpg);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Regina Hebig</div>
+			<div class="affiliation">Chalmers and University of Gothenburg, Sweden</div>
+		</div>
+	</div>
+	
+
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/person.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Rashidah Kasauli Namisanvu</div>
+			<div class="affiliation">Makerere University, Uganda</div>
+		</div>
+	</div>
+	
+
+
+	
+	
+
+
+	
+
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/regina.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Regina Hebig</div>
+			<div class="affiliation">Chalmers and University of Gothenburg, Sweden</div>
+		</div>
+	</div>
+	
+	
+
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/person.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Leonard Peter Binamungu</div>
+			<div class="affiliation">Department of Computer Science and Engineering, College of ICT, University of Dar es Salaam, Tanzania</div>
+		</div>
+	</div>
+	
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/michel.jpg);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Michel Chaudron</div>
+			<div class="role">General Chair</div>
+			<div class="affiliation">Chalmers and University of Gothenburg, Sweden</div>
+		</div>
+	</div>
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/engineer.jpg);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Engineer Bainomugisha</div>
+			<div class="role">General Chair</div>
+			<div class="affiliation">Makerere University, Uganda</div>
+		</div>
+	</div>
+	
+	
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/regina.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Foutse Khomh</div>
+			<div class="affiliation">DGIGL, École Polytechnique de Montréal, Cananda</div>
+		</div>
+	</div>
+	
+	<div class="spacer"></div>
+	
 </div>
 
 <?php require 'right-bar-footer.php'; //THE MUST BE INCLUDED FOR EACH PAGE ?>
